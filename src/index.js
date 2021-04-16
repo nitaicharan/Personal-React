8b4df966264a93b1851caafe2b1f4cfbd7bb90b4
Nitai Charan <nitaicharan@gmail.com>
2021-04-16 00:00:00 -0300

    3. Rendering Elements
    https://reactjs.org/docs/rendering-elements.html

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ import './index.css';
 const name = 'Nitai Charan';
 const element = <h1>Hello, {name}</h1>;
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  // highlight-next-line
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
