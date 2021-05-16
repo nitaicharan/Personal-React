f8a8269f71ac2d0264a454f0d8dc110f94b5d950
Nitai Charan <nitaicharan@gmail.com>
2021-05-16 00:00:00 -0300

    Added redux-thunk dependency

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,11 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import * as reducers from "./reducers/index";
+import thunk from 'redux-thunk';
 
-export const store = createStore(combineReducers({ ...reducers }), composeWithDevTools());
+export const store = createStore(
+    combineReducers({ ...reducers }),
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    ),
+);
